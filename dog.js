0717a35afe308e26048596f7f855b8c15f875b3a
dongjaelee <milkbear0313@gmail.com>
2021-11-11 12:58:26 +0900

    modified

--- dog.js
+++ dog.js
@@ -3,14 +3,46 @@ const fs = require('fs')
 
 const dogRouter = express.Router()
 
-for(let i=0;i<5;i++){
-    dogRouter.get(`/${i}`, (req,res)=>{
-        const filedir = `./dog/dog${i}.jpg`
-        fs.readFile(filedir, (err,data)=>{
-            res.write(data);
-            res.end()
-        })
+const breeds = ['shiba', 'terrier', 'retriever'];
+dogRouter.get(`/dog/:breed/:index.jpg`, (req,res)=>{
+    const filedir = `./dog/${req.params.breed}/${req.params.index}.jpg`
+    fs.readFile(filedir, (err,data)=>{
+        res.write(data);
+        res.end()
     })
-}
+})
+dogRouter.get(`/:breed/images/random/:number`, (req,res)=>{
+    var data = new Object()
+    data.status = 'success'
+    let fileList = []
+    data.message = fileList
+    if(breeds.includes(req.params.breed)){
+        if(isNaN(req.params.number)){
+            data.status = 'fail: not a number'
+            res.send(data)
+            res.end()
+        }
+        for(let i=0;i<parseInt(req.params.number);i++){
+            if(i>4){
+                data.status = 'fail:over the index'
+                data.message = fileList;
+                let jsonData = JSON.stringify(data)
+                res.send(jsonData)
+                res.end()
+            }
+            const filedir = `http://localhost:3000/api/breed/dog/${req.params.breed}/${i}.jpg`            
+            fileList.push(filedir)
+        }
+        data.message = fileList;
+        let jsonData = JSON.stringify(data)
+        res.send(jsonData)
+        res.end()
+    }
+    else{
+        data.status = `fail: no picture for ${req.params.breed}`
+        res.send(data)
+        res.end()
+    }
+})
 
 module.exports = dogRouter
--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express')
 const fs = require('fs')
 const dogRouter = require('./dog.js')
-const catRouter = require('./cat.js')
 
 
 const app = express()
 const port = 3000
 
 
-app.use('/dog', dogRouter)
-app.use('/cat', catRouter)
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.use('/api/breed', dogRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
